0fd284ea5c77d266e14269d91cfd91013f78665b
Legend Gamerz <75073704+ANIKETEDITZ@users.noreply.github.com>
2024-01-04 20:28:05 +0530

    Update test.js

--- src/commands/others/test.js
+++ src/commands/others/test.js
@@ -1,1 +1,9 @@
 const { SlashCommandBuilder } = require('@discord.js/builders');
+
+module.exports = {
+data: new SlashCommandBuilder()
+  .setname('test')
+  .setDescription('Prabhjot is the most useless teen ever'),
+  async execute(interaction, client) {
+    
+}

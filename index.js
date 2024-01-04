f30b407dee2f0d5898c320cc8b3a8871524558d9
Legend Gamerz <75073704+ANIKETEDITZ@users.noreply.github.com>
2024-01-04 19:43:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,13 +11,33 @@ client.on("ready", () => {
 
 });
 
+client.on("messageCreate", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+
+  const command = args.shift().toLowerCase;
+
+
+  //message array
+
+  const messageArray = message.content.split(" ")
+  const argument = messageArray.slice(1);
+  const cmd = messageArray[0];
+
+  //CMDS
+//TEST CMDS
+
+if ( command === 'test' ) {
+  messagw.channel.send(" मैं अब  discord.js v14 पर आगया लेकिन प्रभजोत अभी भी सुतिया है ")
+}
+
+  
 
 
 
 
-
-
-
+})
 
 
 
